3c8741a0f1bef1390b366a372758b641a76b7935
Tristan L <tristan97little@gmail.com>
2023-01-10 16:50:43 -0700

    Create database and schema for storing user timezones

---
+++ db/index-template.js
@@ -0,0 +1,15 @@
+// const { Pool } = require('pg');
+
+// const pool = new Pool({
+//   user: 'FIXME'
+//   host: 'localhost',
+//   port: 5432,
+//   password: 'FIXME'// TODO: CreatePassword,
+//   database: 'FIXME' //TODO: NameDatabase,
+// });
+
+// module.exports.db = {
+//   query: (text, params, callback) => (
+//     pool.query(text, params, callback)
+//   )
+// };
