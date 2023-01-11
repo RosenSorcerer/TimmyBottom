89f4e2364190d0bca05958c5dc827345791de56b
Tristan L <tristan97little@gmail.com>
2023-01-10 19:51:12 -0700

    Updated template

--- db/index-template.js
+++ db/index-template.js
@@ -1,7 +1,7 @@
 // const { Pool } = require('pg');
 
 // const pool = new Pool({
-//   user: 'FIXME'
+//   user: 'FIXME' //TODO: NameUser,
 //   host: 'localhost',
 //   port: 5432,
 //   password: 'FIXME'// TODO: CreatePassword,
