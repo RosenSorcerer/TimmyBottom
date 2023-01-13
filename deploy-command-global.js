844ac3c4c095da74cc32e79e72b6985fcf23b264
Tristan L <tristan97little@gmail.com>
2023-01-13 14:19:13 -0700

    Allowed deployment of commands globally

---
+++ deploy-command-global.js
@@ -0,0 +1,30 @@
+const { REST, Routes } = require('discord.js');
+const { clientId, guildId, token } = require('./config.json');
+const fs = require('node:fs');
+
+const commands = [];
+// Grab all command files from the commands directory
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
+}
+
+//Construct and prepare a REST module instance
+const rest = new REST({ version: '10' }).setToken(token);
+
+(async() => {
+  try {
+    console.log(`Started refreshing ${commands.length} application slash commands!`);
+
+    const data = await rest.put(
+      Routes.applicationCommands(clientId),
+      { body: commands }
+    );
+    console.log(`Successfully reloaded ${data.length} application slash commands!~`);
+
+  } catch (error) {
+    console.error(error);
+  }
+})();
